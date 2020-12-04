fbea3992f43c329ee6829f89338e03c9acab6020
ilkimo <vibertikim@yahoo.it>
2020-12-04 10:33:11 +0100

    chore: add progress

---
+++ src/Main.java
@@ -0,0 +1,229 @@
+import lib_password_management.PasswordManager;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    private static final String REGISTER_ACCOUNT = "1";
+    private static final String LOGIN = "2";
+    private static final String USERS_SEPARATOR = ";";
+    private static final String USER_DATA_SEPARATOR = " ";
+    private static final String USER_STOP_EVENT_MESSAGE = "user wants to exit";
+    private static final String ACCOUNT_FILE = "passwords.txt";
+    private static final String GOODBYE_MESSAGE = "bye bye motherfucker!";
+    
+    /**
+     * just a toy for trying to create accounts and make log in-s and verify that the
+     * data gets inserted in the file represented by String ACCOUNT_FILE = "passwords.txt"
+     * @param args
+     */
+    public static void main(String[] args) {
+        String[] strings_accounts = null;
+        ArrayList<PasswordManager.UserData> accounts = new ArrayList<>();
+        String choice = "";
+        
+        try {
+            strings_accounts = readFile(ACCOUNT_FILE).split(USERS_SEPARATOR);
+            
+            for(String str : strings_accounts) {
+                String[] values = str.split(USER_DATA_SEPARATOR);
+                
+                if(values != null && values.length == 3) {
+                    accounts.add(new PasswordManager.UserData(values[0], values[1], values[2], true, USER_DATA_SEPARATOR, USERS_SEPARATOR));
+                } else {
+                    // ignore wrong line
+                }
+            }
+            
+            choice = askChoice();
+    
+            while(choice.equals(REGISTER_ACCOUNT) || choice.equals(LOGIN)) {
+                switch(choice) {
+                    case REGISTER_ACCOUNT -> {
+                        if(register(accounts)) {
+                            System.out.println("Registration complete! Inserted in file " + ACCOUNT_FILE);
+                        }
+                    }
+                    case LOGIN -> {
+                        if(login(accounts)) {
+                            System.out.println("YOU LOGGED IN SUCCESFULLY!");
+                        }
+                    }
+                }
+            
+                choice = askChoice();
+            }
+            
+            System.out.println(GOODBYE_MESSAGE);
+        } catch(Exception e) {
+            if(e.getMessage().contains(USER_STOP_EVENT_MESSAGE)) {
+                System.out.println(GOODBYE_MESSAGE);
+            } else {
+                e.printStackTrace();
+                throw new Error(e.getMessage());
+            }
+        }
+    }
+    
+    private static boolean register(ArrayList<PasswordManager.UserData> accounts) throws Exception {
+        boolean success = false;
+        
+        if(accounts != null) {
+            PasswordManager.UserData user = getUserData(accounts);
+            accounts.add(user);
+            
+            saveOnFile(user, ACCOUNT_FILE);
+            success = true;
+        } else {
+            throw new Error("Error: accounts is null pointer!");
+        }
+        
+        return success;
+    }
+    
+    private static boolean login(ArrayList<PasswordManager.UserData> accounts) {
+        if(accounts != null) {
+            //TODO
+        } else {
+            throw new Error("Error: accounts is null pointer!");
+        }
+        
+        return false; //TODO
+    }
+    
+    private static String askChoice() {
+        Scanner t = null;
+        String choice= "";
+        
+        try {
+            t = new Scanner(System.in);
+    
+            System.out.println("What do you want to do?");
+            System.out.println("\t" + REGISTER_ACCOUNT + " --> register account");
+            System.out.println("\t" + LOGIN + " --> log-in existing account");
+            System.out.println("\tanything else --> exit");
+    
+            System.out.print("choose: ");
+            
+            choice = t.nextLine();
+        } catch(Exception e) {
+            throw e;
+        } finally {
+            if(t != null) {
+                t.close();
+            }
+        }
+    
+        return choice;
+    }
+    
+    private static String readFile(String filename) {
+        String file_content = "";
+        Scanner inputStream = null;
+        
+        try {
+            inputStream = new Scanner(new File(filename));
+            
+            while(inputStream.hasNextLine()) {
+                file_content += inputStream.nextLine();
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+            throw new Error(e.getMessage());
+        } finally {
+            if(inputStream != null) {
+                inputStream.close();
+            }
+        }
+        
+        return file_content;
+    }
+    
+    private static PasswordManager.UserData getUserData(ArrayList<PasswordManager.UserData> accounts) throws Exception {
+        String ID, password;
+        
+        ID = getID(accounts);
+        password = getPassword();
+        return new PasswordManager.UserData(ID, password, false, USER_DATA_SEPARATOR, USERS_SEPARATOR);
+    }
+    
+    private static String getID(ArrayList<PasswordManager.UserData> accounts) throws Exception {
+        String ID = "";
+        Scanner keyboard = null;
+        
+        try {
+            keyboard = new Scanner(System.in);
+    
+            //keyboard.nextLine(); //to throw rubbish away //TODO cancellami
+            
+            System.out.print("Insert ID (write 'EXIT' to exit): ");
+            ID = keyboard.nextLine();
+            System.out.println("e fin qui ci siamo"); //TODO CANCELLAMI
+            while(!((ID.equalsIgnoreCase("EXIT")) | (validID(ID, accounts)))) {
+                System.out.print("this ID already exists, try another (write 'EXIT' to exit): ");
+                ID = keyboard.nextLine();
+            }
+    
+            if(ID.equalsIgnoreCase("EXIT")) {
+                throw new Exception("Exception: " + USER_STOP_EVENT_MESSAGE);
+            }
+        } catch(Exception e) {
+            throw e;
+        } finally {
+            if(keyboard != null) {
+                keyboard.close();
+            }
+        }
+        
+        return ID;
+    }
+    
+    private static String getPassword() {
+        String password = "";
+        Scanner t = null;
+    
+        try {
+            t = new Scanner(System.in);
+    
+            System.out.print("Insert password: ");
+            password = t.nextLine();
+        } catch(Exception e) {
+        
+        } finally {
+            if(t != null) {
+                t.close();
+            }
+        }
+        
+        return password;
+    }
+    
+    private static boolean validID(String ID, ArrayList<PasswordManager.UserData> accounts) {
+        if(accounts.size() == 0) {
+            return true;
+        } else {
+            return accounts.indexOf(new PasswordManager.UserData(ID, "password", false, "", "")) != -1;
+        }
+    }
+    
+    private static void saveOnFile(PasswordManager.UserData user, String filename) {
+        PrintWriter outputStream = null;
+        
+        try {
+            outputStream = new PrintWriter(filename);
+            
+            outputStream.println(user.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new Error(e.getMessage());
+        } finally {
+            if(outputStream != null) {
+                outputStream.flush();
+                outputStream.close();
+            }
+        }
+    }
+}
---
+++ src/lib_password_management/PasswordManager.java
@@ -0,0 +1,144 @@
+package lib_password_management;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class PasswordManager {
+    private static final String ALGORYTHM = "SHA-256";
+    private static final int SALT_DIM = 16;
+    
+    private static String hash(String password, String salt, String users_separator, String user_data_separator) {
+        MessageDigest md;
+        byte[] hashed_password;
+        StringBuilder sb;
+    
+        try {
+            // Select the message digest for the hash computation -> SHA-256
+            md = MessageDigest.getInstance(ALGORYTHM);
+    
+            // Passing the salt to the digest for the computation
+            md.update(salt.getBytes(StandardCharsets.UTF_8));
+    
+            // Generate the salted hash
+            hashed_password = md.digest(password.getBytes(StandardCharsets.UTF_8));
+    
+            sb = new StringBuilder();
+            for(byte b : hashed_password) {
+                sb.append(String.format("%02x", b));
+            }
+        } catch(NoSuchAlgorithmException e) {
+            throw new Error("Error: no such algorythm --> " + ALGORYTHM);
+        }
+        
+        String res = sb.toString();
+        res.replaceAll(users_separator, "");
+        res.replaceAll(user_data_separator, "");
+        
+        return res;
+    }
+    
+    public static String getRandomSalt(String users_separator, String user_data_separator) {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[SALT_DIM];
+        random.nextBytes(salt);
+        
+        String res = new String(salt);
+        res.replaceAll(users_separator, "");
+        res.replaceAll(user_data_separator, "");
+        
+        return res;
+    }
+    
+    public static class UserData implements Comparable {
+        private String ID;
+        private String salt;
+        private String hashed_password;
+        private String user_data_separator;
+        private String users_separator;
+        
+        public UserData(String ID, String password, boolean password_already_hashed, String user_data_separator, String users_separator) {
+            this.ID = ID;
+            this.user_data_separator = user_data_separator;
+            this.users_separator = users_separator;
+            
+            if(password_already_hashed) {
+                hashed_password = password;
+            } else {
+                setPassword(password, true);
+            }
+        }
+    
+        public UserData(String ID, String salt, String password, boolean password_already_hashed, String user_data_separator, String users_separator) {
+            this.ID = ID;
+            this.salt = salt;
+            this.user_data_separator = user_data_separator;
+            this.users_separator = users_separator;
+            
+            if(password_already_hashed) {
+                hashed_password = password;
+            } else {
+                setPassword(password, false);
+            }
+        }
+    
+        /**
+         * hashes the given password and saves the hashed_password in the object attributes,
+         * if generateRandomSalt is true, the salt gets generated randomly, otherwise the
+         * actual value of salt is used
+         * @param password
+         * @param generateRandomSalt
+         */
+        public void setPassword(String password, boolean generateRandomSalt) {
+            if(generateRandomSalt) {
+                salt = getRandomSalt(users_separator, user_data_separator);
+            }
+            
+            hashed_password = hash(password, salt, users_separator, user_data_separator);
+        }
+        
+        public String getID() { return ID; }
+        public String getSalt() { return salt; }
+        public String getHashedPassword() { return hashed_password; }
+    
+        @Override
+        public boolean equals(Object obj) {
+            boolean equals = false;
+            
+            if(obj != null) {
+                System.out.println("PERFECT1! REMOVE ME class name " + obj.getClass().getName());//TODO cancellami
+                if(obj.getClass().getName().equals("lib_password_management.PasswordManager$UserData")) {
+                    equals = ID.equals(((PasswordManager.UserData) obj).getID());
+                }
+            } else {
+                System.out.println("shit1");//TODO cancellami
+            }
+            
+            return equals;
+        }
+    
+        @Override
+        public int compareTo(Object other) {
+            int comparison = -1;
+            
+            if(other != null) {
+                System.out.println("PERFECT2! REMOVE ME class name " + other.getClass().getName());//TODO cancellami
+                if(other.getClass().getName().equals("lib_password_management.PasswordManager$UserData")) {
+                    comparison = ID.compareTo(((PasswordManager.UserData) other).getID());
+                }
+            } else {
+                System.out.println("shit2");//TODO cancellami
+            }
+            
+            return comparison;
+        }
+    
+        @Override
+        public String toString() {
+            return ID + user_data_separator +
+                    salt + user_data_separator +
+                    hashed_password + users_separator;
+        }
+    }
+}
